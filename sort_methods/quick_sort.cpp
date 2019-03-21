4cbd9ecc115f12af8eb31bee014f1d87350812fb
Mrcer <2969944867@qq.com>
2019-03-21 08:50:23 +0000

    Stupid of me

--- sort_methods/quick_sort.cpp
+++ sort_methods/quick_sort.cpp
@@ -1,38 +1,34 @@
 #include "../main.hpp"
 using namespace std;
 
-string Element::sort_name{"heap_sort"};
-
-void heapify(vector<Element> &arr, int fa, int &heap_len) {
-	int left = 2 * fa + 1;
-	int right = 2 * fa + 2;
-	int max = fa;
-	if(left < heap_len && arr[left] > arr[max])
-		max = left;
-	if(right < heap_len && arr[right] > arr[max])
-		max = right;
-	if(max != fa) {
-		Element::swap(arr[max], arr[fa]);
-		heapify(arr, max, heap_len);
-	}
+string Element::sort_name{"quick_Sort"};
+ 
+int Paritition(vector<Element> &vec, int low, int high) {
+   Element pivot = vec[low];
+   while (low < high) {
+     while (low < high && vec[high] >= pivot) {
+       --high;
+     }
+     vec[low] = vec[high];
+     while (low < high && vec[low] <= pivot) {
+       ++low;
+     }
+     vec[high] = vec[low];
+   }
+   vec[low] = pivot;
+   return low;
 }
 
-void build_max_heap(vector<Element> &arr) {
-	int len = arr.size();
-	for(int i = arr.size() / 2 - 1; i >= 0; --i)
-		heapify(arr, i, len);
-}
-
-void heap_sort(vector<Element> &arr) {
-	build_max_heap(arr);
-	int heap_len = arr.size();
-	while(heap_len > 1) {
-		Element::swap(arr[0], arr[heap_len - 1]);
-		--heap_len;
-		heapify(arr, 0, heap_len);
-	}
-}
+ void quickSort(vector<Element> &vec, int low, int high)
+ {
+   if (low < high) {
+     int pivot = Paritition(vec, low, high);
+     quickSort(vec, low, pivot - 1);
+     quickSort(vec, pivot + 1, high);
+   }
+ }
 
 void Element::sort_method() {
-	heap_sort(data);
+	quickSort(data, 0, data.size() - 1);	
 }
+
